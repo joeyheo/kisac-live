abdeaa78425898356ef2d0d3fbb5fe3bcc5a138f
Joey Heo <heojoey2004@gmail.com>
2023-08-20 14:05:35 +0900

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBu30PxMT5OAqNi8Ue7yjffm-vKhTu6WYs",
-  authDomain: "kisac-live-production.firebaseapp.com",
-  projectId: "kisac-live-production",
-  storageBucket: "kisac-live-production.appspot.com",
-  messagingSenderId: "812217665656",
-  appId: "1:812217665656:web:761499604f7b8e7a50858d",
-  measurementId: "G-317TFV14H0",
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-",
 };
 
 // Initialize Firebase
